52c57eb266e3e36fa15061e98a193bd1fcb1d0e6
jofarias98 <jofarias98@gmail.com>
2018-01-27 12:24:11 -0300

    atualizado

--- api/obra/obraService.js
+++ api/obra/obraService.js
@@ -1,5 +1,51 @@
 const Obra = require('./obra');
-
+Obra.route('bom.get', (req, res, next) => {
+    Obra.aggregate(
+        [
+            { "$project": {
+                "id_obra": 1,
+                "descricao": 1,
+                "bom": 1,
+                "ruim": 1,
+                "status": 1,
+                "length": { "$size": "$bom" }
+            }},
+            { "$sort": { "length": -1 } },
+            { "$limit": 10 }
+        ],
+        (err,results) => {
+            if(!err){
+                res.send(results)
+                return;
+            }
+            res.status(404).send("erro")
+        }
+    )
+ });
+ Obra.route('ruim.get', (req, res, next) => {
+    Obra.aggregate(
+        [
+            { "$project": {
+                "id_obra": 1,
+                "descricao": 1,
+                "bom": 1,
+                "ruim": 1,
+                "status": 1,
+                "length": { "$size": "$ruim" }
+            }},
+            { "$sort": { "length": -1 } },
+            { "$limit": 10 }
+        ],
+        (err,results) => {
+            if(!err){
+                res.send(results)
+                return;
+            }
+            res.status(404).send("erro")
+        }
+    )
+ });
 Obra.methods(['get', 'post', 'put', 'delete']);
+
 Obra.updateOptions({new: true, runValidators: true});
 module.exports = Obra;
